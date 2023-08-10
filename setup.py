8525df85724bf13bc288a520241d934dd7f65752
jvhart <jarodvhart@gmail.com>
2023-08-10 13:47:27 -0500

    [django-4]  version update

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
